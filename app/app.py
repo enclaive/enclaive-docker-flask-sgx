3317c94d15ac592c83273c8c980b98be9ab41444
ole-hansen <12533040+ole-hansen@users.noreply.github.com>
2023-07-16 21:41:27 +0200

    modify static files

--- app/app.py
+++ app/app.py
@@ -5,14 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    file_content = read_file('/app/static/message')
-    write_to_log_file("RETURN: " + file_content)
-    return file_content + '\n'
-
-def read_file(file_name):
-    with open(file_name, 'r') as file:
-        content = file.read()
-    return content
+    with open('/app/static/message.txt', 'r') as file:
+        file_content = file.read()
+        write_to_log_file("[GET] Sending: " + file_content)
+        return file_content + '\n'
 
 def write_to_log_file(message):
     log_file_path = '/app/data/flask.log'
