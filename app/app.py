293b9f9d54e0524421306a9d5f11688ed8b7763f
ole-hansen <12533040+ole-hansen@users.noreply.github.com>
2023-07-16 22:07:41 +0200

    add static file serving

--- app/app.py
+++ app/app.py
@@ -1,33 +1,17 @@
 from flask import Flask
-from logging.config import dictConfig
-
-dictConfig(
-    {
-        "version": 1,
-        "formatters": {
-            "default": {
-                "format": "[%(asctime)s] %(levelname)s | %(module)s >>> %(message)s",
-            }
-        },
-        "handlers": {
-            "console": {
-                "class": "logging.StreamHandler",
-                "stream": "ext://sys.stdout",
-                "formatter": "default",
-            },
-            "file": {
-                "class": "logging.FileHandler",
-                "filename": "data/flask.log",
-                "formatter": "default",
-            },
-        },
-        "root": {"level": "DEBUG", "handlers": ["console", "file"]},
-    }
-)
+import logging
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    app.logger.warn("Secret Log")
-    return 'Hello World!\n'
+def hello():
+    with open('static/message.txt', 'r') as file:
+        file_content = file.read()
+    write_to_log_file("[GET] Sending: " + file_content)
+    return file_content + '\n'
+
+def write_to_log_file(message):
+    log_file_path = '/app/data/flask.log'
+
+    with open(log_file_path, 'a') as file:
+        file.write(message + '\n')
