57f98e434f50b4657c4510c513f27ea1ec0c1116
ole-hansen <12533040+ole-hansen@users.noreply.github.com>
2023-07-16 21:08:30 +0200

    add static content file

--- app/app.py
+++ app/app.py
@@ -5,9 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    write_to_log_file("Sensitive Log Entry")
-    return 'Hello there!\n'
+    file_content = read_file('/app/static/message')
+    write_to_log_file("RETURN: " + file_content)
+    return file_content + '\n'
 
+def read_file(file_name):
+    with open(file_name, 'r') as file:
+        content = file.read()
+    return content
 
 def write_to_log_file(message):
     log_file_path = '/app/data/flask.log'
